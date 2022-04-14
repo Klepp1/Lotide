af9f253a5d0bfcfc1fae437380c66c0d8bcc3e27
Kyler <kylerlepp8@gmail.com>
2022-04-14 13:09:49 -0700

    added chai tests as requested

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,6 +1,12 @@
-const assertEqual = require('../assertEqual');
 const eqArrays = require('../eqArrays');
+const assert = require('chai').assert;
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 2]), true);
+describe('testing the eqArrays function', () => {
+  it('should return true when given equal arrays [1,2,3] and [1,2,3]', () => {
+    assert.deepEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+  it('should return false when given unequal arrays [1,2,3] and [1,2,2]', () => {
+    assert.deepEqual(eqArrays([1, 2, 3], [1, 2, 2]), false);
+  });
+});
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
